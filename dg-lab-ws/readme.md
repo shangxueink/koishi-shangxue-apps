c2dfae26fb7b59a95d89df4f460287793d5547d7
shangxue <1919892171@qq.com>
2024-10-02 19:10:31 +0800

    Delete dg-lab-ws directory

--- dg-lab-ws/readme.md
+++
@@ -1,5 +0,0 @@
-# koishi-plugin-dg-lab-ws
-
-[![npm](https://img.shields.io/npm/v/koishi-plugin-dg-lab-ws?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-dg-lab-ws)
-
-
--- dg-lab-ws/src/config.ts
+++
@@ -1,9 +0,0 @@
-import { Schema } from "koishi";
-
-export interface Config {
-        path: string
-}
-
-export const Config: Schema<Config> = Schema.object({
-        path: Schema.string().default("/dglab").description("Ws服务器监听路径")
-}).description("Ws服务器配置")
