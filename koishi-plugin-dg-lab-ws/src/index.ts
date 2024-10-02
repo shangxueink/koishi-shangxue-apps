79f3dedece9ca07a0f8ea02351aa54183b13b4ca
shangxue <1919892171@qq.com>
2024-10-03 01:59:17 +0800

    Add files via upload

--- koishi-plugin-dg-lab-ws/src/index.ts
+++ koishi-plugin-dg-lab-ws/src/index.ts
@@ -4,7 +4,20 @@ import {Config} from './config'
 
 export const name = 'dg-lab-ws'
 export * from './config'
+export const usage = `
+<div align="center">
+<img src="https://dungeon-lab.cn/img/icons/u95.png" width="200" height="66">
+</div>
 
+通过安装和配置本插件，
+
+你可以实现公网命令转发和连接管理，
+
+让多个用户共享一个 WebSocket 服务器实例。
+
+- \`path\`：WebSocket 服务器的路径。
+
+`;
 export function apply(ctx: Context, config: Config) {
         // 启动ws服务器
         ctx.plugin(WsServer, config.path)
