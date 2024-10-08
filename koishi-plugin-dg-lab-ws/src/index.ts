10b276192cb5df51f6761e9eff997bad7fa26466
shangxue <1919892171@qq.com>
2024-10-08 23:11:30 +0800

    Add files via upload

--- koishi-plugin-dg-lab-ws/src/index.ts
+++ koishi-plugin-dg-lab-ws/src/index.ts
@@ -5,17 +5,17 @@ import { Config } from './config'
 export const name = 'dg-lab-ws'
 export * from './config'
 export const usage = `
-<div align="center">
-<img src="https://dungeon-lab.cn/img/icons/u95.png" width="200" height="66">
+<div style="text-align: center;">
+<img src="https://dungeon-lab.cn/img/icons/u95.png" width="200" height="66" alt="DG-LAB Logo">
 </div>
 
-通过安装和配置本插件，
+<p>通过安装和配置本插件，你可以实现公网命令转发和连接管理，让多个用户共享一个 WebSocket 服务器实例。</p>
 
-你可以实现公网命令转发和连接管理，
+<ul>
+<li><code>port</code>：WebSocket 服务器的端口。</li>
+</ul>
 
-让多个用户共享一个 WebSocket 服务器实例。
-
-- \`path\`：WebSocket 服务器的路径。
+<p><strong>注意：</strong>本插件提供的服务是一个 WebSocket 服务。与 DG-LAB 的 WebSocket 服务器连接时，强制需要二维码链接。二维码直接对应 WebSocket 服务器的地址，因此本插件可能会泄露服务器 IP。请谨慎考虑后再决定是否开启公开服务。</p>
 
 `;
 export function apply(ctx: Context, config: Config) {
