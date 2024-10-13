c64b61bc0471f06053895180180a96adc768e34b
shangxue <1919892171@qq.com>
2024-10-13 21:37:07 +0800

    Delete koishi-plugin-dg-lab-ws directory

--- koishi-plugin-dg-lab-ws/src/config.ts
+++
@@ -1,9 +0,0 @@
-import { Schema } from "koishi";
-
-export interface Config {
-        port: string
-}
-
-export const Config: Schema<Config> = Schema.object({
-        port: Schema.string().default("5555").description("Ws服务器监听端口")
-}).description("Ws服务器配置")
