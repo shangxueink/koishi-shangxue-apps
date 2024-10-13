e034da8b9249ad7cc1a0e894027855aae6750265
shangxue <1919892171@qq.com>
2024-10-13 18:42:06 +0800

    Delete koishi-plugin-miyoushe-meme directory

--- koishi-plugin-miyoushe-meme/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Context, Schema } from 'koishi';
-export interface Config {
-}
-export declare const name = "miyoushe-meme";
-export declare const Config: Schema<Config>;
-export declare function apply(ctx: Context): void;
