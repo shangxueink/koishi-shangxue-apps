a1f5293d2a1294b8d3470a6b34458fdead9b6e04
shangxue <1919892171@qq.com>
2024-10-15 02:18:49 +0800

    Delete plugins/beta-dxl-bert-vits/lib directory

--- plugins/beta-dxl-bert-vits/lib/constants.d.ts
+++
@@ -1,6 +0,0 @@
-import { APISpeakers } from './types';
-export declare const APISpeakerList: APISpeakers[];
-export declare const SpeakerKeyIdMap: Record<number, string>;
-export declare const SpeakerKeyMap: {
-    [k: string]: string;
-};
--- plugins/beta-dxl-bert-vits/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from 'koishi';
-import { Config } from './config';
-export * from './config';
-export declare function apply(ctx: Context, config: Config): void;
-export declare const inject: {
-    optional: string[];
-};
