17b031a41a6dcc7edc2a4c7c1c93349b0b773aaa
shangxue <1919892171@qq.com>
2024-11-18 03:10:23 +0800

    Add files via upload

--- plugins/patina/lib/index.js
+++ plugins/patina/lib/index.js
@@ -77,6 +77,45 @@ exports.usage = `
 <p>触发指令后会返回图片</p>
 </details>
 
+<details>
+<summary>点击此处查看——鉴黄</summary>
+<p>通过调用 puppeteer 操作网页，模拟图像处理</p>
+<p>如果你需要更详细地了解这个项目，请前往 
+<a href="https://magiconch.com/nsfw/" target="_blank">https://magiconch.com/nsfw/</a>
+</p>
+<h2>功能示例</h2>
+<pre>
+鉴黄 [图片]
+</pre>
+<p>触发指令后会进行鉴黄判断 可能需要10秒左右</p>
+</details>
+
+<details>
+<summary>点击此处查看——幻影坦克</summary>
+<p>如果你需要更详细地了解这个项目，请前往 
+<a href="https://uyanide.github.io/Mirage_Colored/" target="_blank">https://uyanide.github.io/Mirage_Colored/</a>
+<h2>功能示例</h2>
+<ul>
+<li><code>幻影</code>：指令触发后，系统会提示上传两张图片。</li>
+<li><code>幻影 [图片]</code>：上传一张图片作为表图，系统会提示上传里图。</li>
+<li><code>幻影 [图片] [图片]</code>：上传两张图片，分别作为表图和里图。</li>
+<li><code>幻影 QQ号 QQ号</code>：使用两个 QQ 号的头像作为表图和里图。</li>
+<li><code>幻影 @用户 @用户</code>：使用两个用户的头像作为表图和里图。</li>
+</ul>
+<pre>
+幻影坦克  [图片] [图片] -c -s 1200 -w 0.7
+</pre>
+<p>触发指令后会返回处理后的图片。</p>
+<h3>参数说明</h3>
+<ul>
+<li><code>-c</code>：开启全彩输出，关闭则为黑白图。</li>
+<li><code>-s &lt;size&gt;</code>：指定输出图像尺寸，默认值为 1200。</li>
+<li><code>-w &lt;weight&gt;</code>：设置里图混合权重，范围为 0 到 1，默认值为 0.7。</li>
+</ul>
+<p>如果不指定参数，将使用默认配置。</p>
+</details>
+
+
 ---
 
 > 目前就这几个指令 ，以后有什么好玩的再加。
@@ -206,14 +245,10 @@ async function apply(ctx, config) {
         }
     }
 
-    ctx.command("幻影 [img1] [img2]", "制作幻影坦克图片")
+    ctx.command("patina/幻影 [img1] [img2]", "制作幻影坦克图片")
         .alias("幻影坦克")
-        .example("幻影")
-        .example("幻影 [图片]")
-        .example("幻影 [图片] [图片]")
-        .example("幻影 QQ号 QQ号")
-        .example("幻影 @用户 @用户")
-        .option('fullColor', '-c', '全彩输出')
+        .example("幻影").example("幻影 [图片]").example("幻影 [图片] [图片]").example("幻影 QQ号 QQ号").example("幻影 @用户 @用户")
+        .option('color', '-c', '全彩输出')
         .option('size', '-s <size:number>', '输出尺寸')
         .option('weight', '-w <weight:number>', '里图混合权重')
         .action(async ({ session, options }, img1, img2) => {
