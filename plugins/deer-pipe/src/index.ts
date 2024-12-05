72b60331e7e785b64e4e6eb61b1121420bf17d85
shangxue <1919892171@qq.com>
2024-12-05 18:36:40 +0800

    Add files via upload

--- plugins/deer-pipe/src/index.ts
+++ plugins/deer-pipe/src/index.ts
@@ -127,7 +127,7 @@ export const Config: Schema = Schema.intersect([
     cost: Schema.object({
 
       checkin_reward: Schema.array(Schema.object({
-        command: Schema.union(['鹿', "鹿@用户", '补鹿', '戒鹿', "补鹿@用户"]).description("交互指令"),
+        command: Schema.union(['鹿', "鹿@用户", '补鹿', '戒鹿', "补鹿@用户", "戴锁"]).description("交互指令"),
         cost: Schema.number().description("货币变动"),
       })).role('table').description('【获取硬币】本插件指令的货币变动').default(
         [
@@ -150,6 +150,10 @@ export const Config: Schema = Schema.intersect([
           {
             "command": "补鹿@用户",
             "cost": -500
+          },
+          {
+            "command": "戴锁",
+            "cost": -100
           }
         ]
       ),
