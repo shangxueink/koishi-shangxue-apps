5257cf7feb38796761dd38233291c68ef0daf42a
shangxue <1919892171@qq.com>
2025-03-21 23:34:15 +0800

    Add files via upload

--- plugins/gif-reverse/src/index.ts
+++ plugins/gif-reverse/src/index.ts
@@ -13,6 +13,108 @@ export const inject = {
   required: ['http', 'i18n', 'logger', 'ffmpeg', 'canvas']
 }
 
+export const usage = `
+---
+
+<table>
+<thead>
+<tr>
+<th>选项</th>
+<th>简写</th>
+<th>描述</th>
+<th>类型</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td><code>--rebound</code></td>
+<td><code>-b</code></td>
+<td>回弹效果（正放+倒放）</td>
+<td><code>boolean</code></td>
+</tr>
+<tr>
+<td><code>--reverse</code></td>
+<td><code>-r</code></td>
+<td>倒放 GIF</td>
+<td><code>boolean</code></td>
+</tr>
+<tr>
+<td><code>--frame</code></td>
+<td><code>-f</code></td>
+<td>指定处理gif的平均帧间隔</td>
+<td><code>number</code></td>
+</tr>
+<tr>
+<td><code>--slide</code></td>
+<td><code>-l</code></td>
+<td>滑动方向 (上/下/左/右)</td>
+<td><code>string</code></td>
+</tr>
+<tr>
+<td><code>--rotate</code></td>
+<td><code>-o</code></td>
+<td>旋转方向 (顺/逆)</td>
+<td><code>string</code></td>
+</tr>
+<tr>
+<td><code>--turn</code></td>
+<td><code>-t</code></td>
+<td>转向角度 (上/下/左/右/0-360)</td>
+<td><code>string</code></td>
+</tr>
+<tr>
+<td><code>--information</code></td>
+<td><code>-i</code></td>
+<td>显示 GIF 信息</td>
+<td><code>boolean</code></td>
+</tr>
+</tbody>
+</table>
+
+---
+
+<h2>使用示例</h2>
+
+<ul>
+<li><strong>回弹 GIF:</strong>
+<pre><code>gif -b</code></pre>
+</li>
+<li><strong>倒放 GIF:</strong>
+<pre><code>gif -r</code></pre>
+</li>
+<li><strong>指定帧间隔 20ms:</strong>
+<pre><code>gif -f 20</code></pre>
+</li>
+<li><strong>右滑 GIF:</strong>
+<pre><code>gif -l 右</code></pre>
+</li>
+<li><strong>逆时针旋转 GIF:</strong>
+<pre><code>gif -o 逆</code></pre>
+</li>
+<li><strong>转向 30 度:</strong>
+<pre><code>gif -t 30</code></pre>
+</li>
+<li><strong>转向向上:</strong>
+<pre><code>gif -t 上</code></pre>
+</li>
+<li><strong>右上方滑动:</strong>
+<pre><code>gif -l 右 -t 45</code></pre>
+</li>
+<li><strong>顺时针旋转:</strong>
+<pre><code>gif -o 顺</code></pre>
+</li>
+<li><strong>显示 GIF 信息:</strong>
+<pre><code>gif -i</code></pre>
+</li>
+</ul>
+
+
+更多使用方法请使用 <code>gif -h</code> 查看指令用法
+
+---
+
+`;
+
 
 export const Config =
   Schema.intersect([
@@ -24,7 +126,7 @@ export const Config =
       usedReverse: Schema.boolean().default(false).description("开启后，在不指定选项时，默认使用`倒放`效果。<br>关闭后，在不指定选项时，执行`-h`选项查看帮助"),
       outputinformation: Schema.boolean().default(true).description("开启后，在生成图片后，带上图片信息`自动 -i 选项`。<br>否则只会输出处理后的GIF图片"),
       fillcolor: Schema.string().role('color').description("GIF图片的底色。默认透明。").default("rgba(255, 255, 255, 0)"),
-      maxFps: Schema.number().default(50).description("限制输出 GIF 的最大帧率，防止卡顿、掉帧。"), // 添加最大帧率限制
+      maxFps: Schema.number().default(50).description("限制输出 GIF 的最大帧率，防止卡顿、掉帧。<br>一般超过`50 FPS`的`GIF`会掉帧。"), // 添加最大帧率限制
     }).description('进阶设置'),
     Schema.object({
       loggerinfo: Schema.boolean().default(false).description("日志调试模式"),
@@ -84,6 +186,7 @@ export function apply(ctx: Context, config) {
     .example(`转向30度：${config.gifCommand} -t 30`)
     .example(`转向向左上：${config.gifCommand} -t 左上`)
     .example(`45度右滑：${config.gifCommand} -l 右 -t 45`)
+    .example(`顺时针旋转：${config.gifCommand} -o 顺`)
     .example(`显示 GIF 信息: ${config.gifCommand} -i`)
     .action(async ({ session, options }, gif) => {
       let { reverse, rebound, frame, slide, rotate, turn, information } = options
