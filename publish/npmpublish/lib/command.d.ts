7f7ef5ffe4e2f2e09c457a9bc12a6345f18354d5
GitHub Action <action@github.com>
2025-04-17 13:17:19 +0000

    ✔️

--- publish/npmpublish/lib/command.d.ts
+++
@@ -1,4 +0,0 @@
-import { Context } from 'koishi';
-import CommandManager from '.';
-export declare function remove<O, K extends keyof O>(object: O, key: K): O[K];
-export default function (ctx: Context, manager: CommandManager): void;
