e1fca401c3e9a8e8cc3dd12ea1bbebb890f5abd5
shangxue <1919892171@qq.com>
2025-09-10 17:10:32 +0800

    Add files via upload

--- plugins/ademo/readme.md
+++ plugins/ademo/readme.md
@@ -1,1 +1,5 @@
+# koishi-plugin-ademo
 
+[![npm](https://img.shields.io/npm/v/koishi-plugin-ademo?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-ademo)
+
+ademo
---
+++ plugins/ademo/src/index.ts
@@ -0,0 +1,37 @@
+import { Context, Logger, Schema, Session } from 'koishi'
+
+export const name = 'ademo'
+export const reusable = false
+export const filter = true
+
+export const inject = {
+  optional: [''],
+  required: ['']
+}
+
+export const usage = `
+`;
+
+const logger = new Logger(`DEV:${name}`);
+
+export interface Config {
+  loggerinfo?: boolean;
+}
+
+export const Config: Schema<Config> = Schema.intersect([
+  Schema.object({
+  }).description("基础设置"),
+  Schema.object({
+    loggerinfo: Schema.boolean().default(false).description("日志调试模式").experimental(),
+  }).description("调试设置"),
+])
+
+export function apply(ctx: Context, config: Config) {
+  // write your plugin here
+
+  function logInfo(...args: any[]) {
+    if (config.loggerinfo) {
+      (logger.info as (...args: any[]) => void)(...args);
+    }
+  }
+}
