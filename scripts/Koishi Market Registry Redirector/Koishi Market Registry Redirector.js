ebdde112d1b3ffcc5e04bff282567f6e6795d5fc
shangxueink <1919892171@qq.com>
2025-11-06 13:04:25 +0800

    bump

--- scripts/Koishi Market Registry Redirector/Koishi Market Registry Redirector.js
+++ scripts/Koishi Market Registry Redirector/Koishi Market Registry Redirector.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Koishi Market Registry Redirector
 // @namespace    https://github.com/shangxueink
-// @version      3.30
+// @version      3.31
 // @description  将 Koishi 市场注册表请求重定向到多个备用镜像源，支持自动重试、单独配置每个镜像源的代理请求解决CORS问题，并修复时间显示问题，镜像地址可点击复制，增加返回顶部按钮。
 // @author       shangxueink
 // @license      MIT
