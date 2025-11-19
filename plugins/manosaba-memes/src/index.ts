c32d811573b04c842a3feb24b538793e743a592f
shangxueink <1919892171@qq.com>
2025-11-19 21:39:09 +0800

    init

---
+++ plugins/manosaba-memes/src/index.ts
@@ -0,0 +1,12 @@
+import { Context, Schema } from 'koishi'
+
+export const name = 'manosaba-memes'
+
+export interface Config { }
+
+export const Config: Schema<Config> = Schema.object({})
+
+export function apply(ctx: Context) {
+  // write your plugin here
+
+}
