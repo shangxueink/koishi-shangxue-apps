a6f9a829b99d8fad9eb9bc36f683a77541efcd80
shangxueink <1919892171@qq.com>
2026-08-09 19:34:50 +0800

    文档(transform-before-send): 为配置说明插入换行标签
    在 mode 配置项的描述文本中加入 <br> 标签，
    让 base64 与 assets 两种模式的说明分行展示，
    改善配置界面中的信息排版与可读性。

--- plugins/transform-before-send/src/config.ts
+++ plugins/transform-before-send/src/config.ts
@@ -9,6 +9,6 @@ export interface Config {
 
 export const Config: Schema<Config> = Schema.object({
   mode: Schema.union(['base64', 'assets']).default('base64')
-    .description('base64：直接编码进消息体；assets：转存为公网链接'),
+    .description('base64：直接编码进消息体；<br>assets：转存为公网链接'),
   loggerinfo: Schema.boolean().default(false).description('输出调试日志').experimental(),
 })
