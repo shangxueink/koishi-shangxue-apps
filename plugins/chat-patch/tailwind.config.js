f3dbb744dba70a62ba3b48839315706e6524f1e9
shangxueink <1919892171@qq.com>
2026-08-28 00:50:20 +0800

    bump

--- plugins/chat-patch/tailwind.config.js
+++ plugins/chat-patch/tailwind.config.js
@@ -3,9 +3,10 @@ const path = require('path')
 
 module.exports = {
   content: [
-    path.resolve(__dirname, './client/**/*.{vue,js,ts,jsx,tsx}'),
-    path.resolve(__dirname, './client/**/*.vue'),
-    path.resolve(__dirname, './client/**/*.ts'),
+    path.resolve(__dirname, './client/*.{vue,js,ts,jsx,tsx}'),
+    path.resolve(__dirname, './client/icons/**/*.{vue,js,ts,jsx,tsx}'),
+    path.resolve(__dirname, './client/vue/**/*.{vue,js,ts,jsx,tsx}'),
+    path.resolve(__dirname, './client/web/src/**/*.{vue,js,ts,jsx,tsx}'),
   ],
   theme: {
     extend: {},
